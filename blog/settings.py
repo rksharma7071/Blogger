c66544dd0a527914504994b52832423f59946e0a
rksharma7071 <rksharma7071@gmail.com>
2023-07-17 11:58:39 +0530

    Blogger 1.3

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$(7ecl!o_pn#+dw^54*qhlh0*ssu(9(dn_f_(&x-juj)r(c+5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
